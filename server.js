07d35fd4e17d4ac0dd5cffc338cf2200b337e580
BeakySneaky <alema027@hotmail.fr>
2020-04-23 17:47:12 -0400

    Switching from Express to NodeJs Server + Websocket connection.

---
+++ server.js
@@ -0,0 +1,59 @@
+let webSocketsServerPort = 3002
+let webSocketServer = require('websocket').server
+let http = require('http')
+let wol = require('node-wol')
+// Spinning the http server and the websocket server.
+let server = http.createServer()
+server.listen(webSocketsServerPort, () => {
+	console.log('Server running on port ' + webSocketsServerPort)
+})
+
+let wsServer = new webSocketServer({
+	httpServer: server,
+})
+
+// I'm maintaining all active connections in this object
+let clients = []
+
+// This code generates unique userid for everyuser.
+const getUniqueID = () => {
+	const s4 = () =>
+		Math.floor((1 + Math.random()) * 0x10000)
+			.toString(16)
+			.substring(1)
+	return s4() + s4() + '-' + s4()
+}
+
+wsServer.on('request', function (request) {
+	//let userID = getUniqueID()
+	//let userID = "user" + clients.length + 1
+	//console.log(userID)
+	console.log(new Date() + ' Recieved a new connection from origin ' + request.origin + '.')
+	let connection = request.accept(null, request.origin)
+	//clients[userID] = connection
+	var index = clients.push(connection) - 1;
+	console.log('connected: ' + index + ' in ' + Object.getOwnPropertyNames(clients))
+	connection.on('message', function (message) {
+		if (message.type === 'utf8') {
+			switch (message.utf8Data) {
+				case 'WOL':
+					wol.wake('30-9C-23-02-C8-AB', function(error){
+						if(error){
+							console.log('error')
+						}else{
+							console.log('success.... For now.')
+							connection.sendUTF("WOL_Success")
+						}
+					})
+					break
+				default:
+			}
+		}
+	})
+
+	connection.on('close', function(){
+		console.log((new Date())+ " Peer " + index + " disconnected.")
+		clients.splice(index, 1);
+
+	})
+})
--- src/App.js
+++ src/App.js
@@ -1,16 +1,85 @@
 import React from 'react'
 import SnackBar from './SnackBar'
 import './App.css'
+import { w3cwebsocket as WebSocket } from 'websocket'
 
 export default class App extends React.Component {
 	constructor() {
 		super()
-		this.getExpressServerStatus()
 		this.state = {
 			SnackBarOn: false,
 			SnackBarText: null,
-			WakeOnLanAvailable: false
+			WakeOnLanAvailable: false,
+			Ws: null,
 		}
+	}
+
+	componentDidMount() {
+		this.connect()
+	}
+
+	connect = () => {
+		var ws = new WebSocket('ws://127.0.0.1:3002')
+		let that = this
+		var connectInterval
+
+		// websocket onopen event listener
+		ws.onopen = () => {
+			console.log('Connected Websocket to Server')
+			this.setState({ WakeOnLanAvailable: true })
+			this.setState({ ws: ws })
+
+			that.timeout = 250
+			clearTimeout(connectInterval)
+		}
+		ws.onmessage = (evt) => {
+			// listen to data sent from the websocket server
+			const message = evt.data
+			if(message === "WOL_Success"){
+				this.sendSnackBar('WOL request sent !')
+			}
+			console.log(message)
+		}
+
+		// websocket onclose event listener
+		ws.onclose = (e) => {
+			this.setState({ WakeOnLanAvailable: false })
+			console.log(
+				`Socket is closed. Attempt to reconnect in ${Math.min(
+					10000 / 1000,
+					(that.timeout + that.timeout) / 1000
+				)} second.`,
+				e.reason
+			)
+
+			that.timeout = that.timeout + that.timeout //increment retry interval
+			connectInterval = setTimeout(this.check, Math.min(10000, that.timeout)) //call check function after timeout
+		}
+
+		// websocket onerror event listener
+		ws.onerror = (err) => {
+			console.error('Socket encountered error: ', err.message, 'Closing socket')
+
+			ws.close()
+		}
+	}
+
+	check = () => {
+		const { ws } = this.state
+		if (!ws || ws.readyState === WebSocket.CLOSED) this.connect() //check if websocket instance is closed, if so call `connect` function.
+	}
+
+	wol = () => {
+		const { ws } = this.state
+		ws.send('WOL')
+		//this.sendSnackBar('WOL request sent !')
+	}
+
+	sendSnackBar = (message) => {
+		setTimeout(() => {
+			this.setState({ SnackBarOn: false })
+		}, 3000)
+		this.setState({ SnackBarOn: true, SnackBarText: message })
 	}
 
 	render() {
@@ -28,33 +97,5 @@ export default class App extends React.Component {
 		} else {
 			return null
 		}
-	}
-
-	wol = () => {
-		fetch('http://localhost:3002/WOL', {
-			method: 'POST'
-		}).then(() => {
-			this.sendSnackBar('WOL request sent !')
-		})
-	}
-
-	getExpressServerStatus = () => {
-		fetch('http://localhost:3002/status', {
-			method: 'GET'
-		})
-			.then((response) => {
-				this.setState({ WakeOnLanAvailable: true })
-			})
-			.catch((err) => {
-				console.log(err)
-			})
-		return true
-	}
-
-	sendSnackBar = (message) => {
-		setTimeout(() => {
-			this.setState({ SnackBarOn: false })
-		}, 3000)
-		this.setState({ SnackBarOn: true, SnackBarText: message })
 	}
 }
